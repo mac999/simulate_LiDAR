0e137922cd4b59eac09b5d4b4e04a69f9dfcc487
Tae wook <laputa99999@gmail.com>
2023-11-08 09:40:41 -0500

    Add files via upload

--- simulate_LiDAR.py
+++ simulate_LiDAR.py
@@ -2,166 +2,201 @@
 # author: taewook kang
 # email: laputa99999@gmail.com
 # version: draft. testing. 
+#  0.1. updated. 
 # todo: load RGBD dataset, Etc.
 # 
+import os, sys, argparse, json, re, math, traceback
 import trimesh, traceback
 import numpy as np
 from tqdm import tqdm
 import open3d as o3d
 
-def main():
-	# test on a sphere mesh
-	# mesh = trimesh.primitives.Sphere(radius = 1.0)
-	mesh = trimesh.primitives.Box(extents = [5.0, 5.0, 3.0])
-	
+def create_box(extents = [5.0, 5.0, 3.0]):
+	mesh = trimesh.primitives.Box(extents)
+	return mesh
+
+def load_mesh(filename):
+	mesh = trimesh.load(filename)
+	return mesh
+
+def create_plane_targets(plane_y):
 	point_targets = []
 	for i in range(50):
 		for j in range(50):
 			step = 0.1
 			x = i * step - 2.5
-			y = 2.5
+			y = plane_y
 			z = j * step - 2.5
 			target = trimesh.primitives.Box(extents = [0.1, 0.1, 0.1], transform = trimesh.transformations.translation_matrix([x, y, z]))
 			# target = trimesh.primitives.Plane() TBD
 			point_targets.append(target)
+	return point_targets
 
+class lidar_device:
 	# Define LiDAR parameters
-	lidar_position = [0.0, 0.0, 0.0]  # LiDAR's position in the room
-	lidar_yaw = 0.0  # LiDAR's yaw angle (rotation around the vertical axis)
-	lidar_fov = 60.0 # 10 # 60  # LiDAR's field of view in degrees
-	lidar_range = 10.0  # Maximum LiDAR range
+	position = [0.0, 0.0, 0.0]  # LiDAR's position in the room
+	yaw = 0.0  # LiDAR's yaw angle (rotation around the vertical axis)
+	fov = 60.0 # 10 # 60  # LiDAR's field of view in degrees
+	range = 10.0  # Maximum LiDAR range
 
-	# create some rays
-	ray_origins = None # np.array([[0.0, 0.5, 0.0]])
-	ray_directions = None # np.array([[0.0, 0.5, 2.0]])
-	
-	# Generate LiDAR point cloud
-	lidar_pcd = o3d.geometry.PointCloud()
-	for azimuth in np.linspace(lidar_yaw - lidar_fov / 2, lidar_yaw + lidar_fov / 2, 10): #360):
-		for elevation in np.linspace(-lidar_fov / 2, lidar_fov / 2, 10): # 180):
-			# Compute laser beam direction in LiDAR's local coordinates
-			direction = np.array([
-				np.cos(np.radians(elevation)) * np.sin(np.radians(azimuth)),
-				np.cos(np.radians(elevation)) * np.cos(np.radians(azimuth)),
-				np.sin(np.radians(elevation)),
-			])
+	def init(self, pos=[0.0,0.0,0.0], yaw=0.0, fov_angle=60.0, range_angle = 10):
+		self.position = pos
+		self.yaw = yaw
+		self.fov = fov_angle
+		self.range = range_angle
 
-			# Compute the end point of the laser beam
-			end_point = np.array([
-				lidar_position[0] + direction[0] * lidar_range,
-				lidar_position[1] + direction[1] * lidar_range,
-				lidar_position[2] + direction[2] * lidar_range,
-			])
+	def create_rays(self):
+		ray_origins = None # np.array([[0.0, 0.5, 0.0]])
+		ray_directions = None # np.array([[0.0, 0.5, 2.0]])
+		
+		# Generate LiDAR point cloud
+		lidar_pcd = o3d.geometry.PointCloud()
+		for azimuth in np.linspace(self.yaw - self.fov / 2, self.yaw + self.fov / 2, 10): #360):
+			for elevation in np.linspace(-self.fov / 2, self.fov / 2, 10): # 180):
+				# Compute laser beam direction in LiDAR's local coordinates
+				direction = np.array([
+					np.cos(np.radians(elevation)) * np.sin(np.radians(azimuth)),
+					np.cos(np.radians(elevation)) * np.cos(np.radians(azimuth)),
+					np.sin(np.radians(elevation)),
+				])
 
-			if ray_origins is None:
-				ray_origins = np.array([lidar_position])
-				ray_directions = np.array([end_point - lidar_position])
-			else:
-				ray_origins = np.append(ray_origins, lidar_position)
-				ray_directions = np.append(ray_directions, end_point - lidar_position)
-	
-	ray_origins = ray_origins.reshape(-1, 3)
-	ray_directions = ray_directions.reshape(-1, 3)
-	
-	# run the mesh-ray test
-	ray_lines_set = []
-	intersection_points_set = []
-	for target in tqdm(point_targets):
-		vertices = target.vertices.copy()  # Make a copy of the vertices array
-		faces = target.faces.copy()        # Make a copy of the faces array
-		m = o3d.geometry.TriangleMesh()
-		m.vertices = o3d.utility.Vector3dVector(vertices)
-		m.triangles = o3d.utility.Vector3iVector(faces)        
-	
-		locations, index_ray, index_tri = target.ray.intersects_location(
-			ray_origins=ray_origins,
-			ray_directions=ray_directions)
+				# Compute the end point of the laser beam
+				end_point = np.array([
+					self.position[0] + direction[0] * self.range,
+					self.position[1] + direction[1] * self.range,
+					self.position[2] + direction[2] * self.range,
+				])
 
-		if len(locations) <= 0:
-			continue
+				if ray_origins is None:
+					ray_origins = np.array([self.position])
+					ray_directions = np.array([end_point - self.position])
+				else:
+					ray_origins = np.append(ray_origins, self.position)
+					ray_directions = np.append(ray_directions, end_point - self.position)
+		
+		ray_origins = ray_origins.reshape(-1, 3)
+		ray_directions = ray_directions.reshape(-1, 3)
 
-		# Create an Open3D PointCloud from the intersection points
-		intersection_points = o3d.geometry.PointCloud()
-		intersection_points.points = o3d.utility.Vector3dVector(locations)
-		intersection_points_set.append(intersection_points)
+		return ray_origins, ray_directions
 
-		# Create an Open3D LineSet to visualize rays
-		ray_lines = o3d.geometry.LineSet()
-		ray_lines.points = o3d.utility.Vector3dVector(np.vstack((ray_origins, ray_origins + ray_directions)))
-		num_points = len(ray_origins)
-		line_indices = np.array([[i, i + num_points] for i in range(num_points)])
-		ray_lines.lines = o3d.utility.Vector2iVector(line_indices)    # Create lines between pairs of points. index. 0,1 is the first line, 2,3 is the second line
-		ray_lines_set.append(ray_lines)
+	def scan(self, ray_origins, ray_directions, targets):
+		ray_lines_set = []
+		intersection_points_set = []
+		for target in tqdm(targets):
+			vertices = target.vertices.copy()  # Make a copy of the vertices array
+			faces = target.faces.copy()        # Make a copy of the faces array
+			m = o3d.geometry.TriangleMesh()
+			m.vertices = o3d.utility.Vector3dVector(vertices)
+			m.triangles = o3d.utility.Vector3iVector(faces)        
+		
+			locations, index_ray, index_tri = target.ray.intersects_location(
+				ray_origins=ray_origins,
+				ray_directions=ray_directions)
 
-	# Create an Open3D TriangleMesh from the Trimesh mesh
-	vertices = mesh.vertices.copy()  # Make a copy of the vertices array
-	faces = mesh.faces.copy()        # Make a copy of the faces array
+			if len(locations) <= 0:
+				continue
+
+			# Create an Open3D PointCloud from the intersection points
+			intersection_points = o3d.geometry.PointCloud()
+			intersection_points.points = o3d.utility.Vector3dVector(locations)
+			intersection_points_set.append(intersection_points)
+
+			# Create an Open3D LineSet to visualize rays
+			ray_lines = o3d.geometry.LineSet()
+			ray_lines.points = o3d.utility.Vector3dVector(np.vstack((ray_origins, ray_origins + ray_directions)))
+			num_points = len(ray_origins)
+			line_indices = np.array([[i, i + num_points] for i in range(num_points)])
+			ray_lines.lines = o3d.utility.Vector2iVector(line_indices)    # Create lines between pairs of points. index. 0,1 is the first line, 2,3 is the second line
+			ray_lines_set.append(ray_lines)
+
+		return ray_lines_set, intersection_points_set
+
+def create_material(shader, base_color, thickness = 0.0, transmission = 0.0, absorption_color = [0.0,0.0,0.0], point_size = 0.0):
+	mat = o3d.visualization.rendering.MaterialRecord()
+	mat.shader = shader
+	mat.base_color = base_color
+	mat.base_roughness = 0.0
+	mat.base_reflectance = 0.0
+	mat.base_clearcoat = 1.0
+	mat.thickness = thickness
+	if point_size > 0.0:
+		mat.point_size = point_size
+	mat.transmission = transmission
+	mat.absorption_distance = 10
+	mat.absorption_color = absorption_color
+	return mat
+
+def convert_mesh(mesh):
+	vertices = mesh.vertices.copy()  
+	normals = mesh.vertex_normals
+	faces = mesh.faces.copy()       
 	o3d_mesh = o3d.geometry.TriangleMesh()
 	o3d_mesh.vertices = o3d.utility.Vector3dVector(vertices)
 	o3d_mesh.triangles = o3d.utility.Vector3iVector(faces)
+	o3d_mesh.vertex_normals = o3d.utility.Vector3dVector(normals)
 
-	# Set visualization properties
-	# o3d_mesh.paint_uniform_color([0.7, 0.7, 0.7])  # Set mesh color
-	# intersection_points.paint_uniform_color([1, 0, 0])  # Set intersection points color
-	# ray_lines.colors = o3d.utility.Vector3dVector(np.tile(np.array([0, 0, 1]), (2, 1)))  # Set ray color
+	return o3d_mesh
 
-	# Create an Open3D visualization window
-	mat_point = o3d.visualization.rendering.MaterialRecord()
-	mat_point.shader = 'defaultLit'
-	mat_point.base_color = [1.0, 0, 0, 1.0]
-	mat_point.thickness = 2.0
- 
-	mat_line = o3d.visualization.rendering.MaterialRecord()
-	mat_line.shader = 'defaultLit'
-	mat_line.base_color = [0.0, 0, 1.0, 1.0]
+def main():
+	# arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--input', default='model.obj', help='...')
+	parser.add_argument('--output', default='output.pcd', help='...')
+	args = parser.parse_args()
 
-	mat_tar = o3d.visualization.rendering.MaterialRecord()
-	mat_tar.shader = 'defaultLit'
-	mat_tar.base_color = [1.0, 0, 1.0, 1.0]
-	
-	mat_box = o3d.visualization.rendering.MaterialRecord()
-	# mat_box.shader = 'defaultLitTransparency'
-	mat_box.shader = 'defaultLitSSR'
-	mat_box.base_color = [0.467, 0.467, 0.467, 0.2]
-	mat_box.base_roughness = 0.0
-	mat_box.base_reflectance = 0.0
-	mat_box.base_clearcoat = 1.0
-	mat_box.thickness = 1.0
-	mat_box.transmission = 0.5
-	mat_box.absorption_distance = 10
-	mat_box.absorption_color = [0.5, 0.5, 0.5]
+	# create object
+	mesh = None
+	if len(args.input) > 0:	
+		mesh = load_mesh(args.input)
+	else:
+		mesh = create_box(extents)
+	# extents = [5.0, 5.0, 3.0]
+	extents = mesh.extents
+	point_targets = create_plane_targets(extents[1] / 2.0)
 
-	geoms = [{'name': 'box', 'geometry': o3d_mesh, 'material': mat_box}]
-	
+	# create virtual lidar and scan
+	lidar = lidar_device()
+	lidar.init()
+
+	ray_origins, ray_directions = lidar.create_rays()
+	ray_lines_set, intersection_points_set = lidar.scan(ray_origins, ray_directions, point_targets)
+
+	# room 
+	geoms = []
+	o3d_mesh = convert_mesh(mesh)
+	mat_box = create_material('defaultLitTransparency', base_color=[0.467,0.467,0.467,0.2], thickness=1.0, transmission=0.5, absorption_color=[0.5, 0.5, 0.5])
+	geom = {'name': 'box', 'geometry': o3d_mesh, 'material': mat_box}
+	geoms.append(geom)
+
+	# materials. defaultLit, defaultUnlit, unlitLine, unlitGradient, unlitSolidColor, defaultLitTransparency.
+	# http://www.open3d.org/docs/latest/python_api/open3d.visualization.tensorboard_plugin.summary.add_3d.html
+	mat_line = create_material('unlitSolidColor', base_color=[0.0, 0, 1.0, 1.0], thickness=1.0)
 	for index, ray_lines in enumerate(ray_lines_set):
-		name = 'line' + str(index)
-		
+		name = 'line' + str(index)		
 		ray = {'name': name, 'geometry': ray_lines, 'material': mat_line}
 		geoms.append(ray)
 	
+	mat_point = create_material('unlitSolidColor', base_color=[1.0, 0, 0, 1.0], thickness=1.0) # , point_size=10.0)	
 	for index, intersection_points in enumerate(intersection_points_set):
 		name = 'intpoints' + str(index)
-		
 		pt = {'name': name, 'geometry': intersection_points, 'material': mat_point}
 		geoms.append(pt)
  
-	'''
+	mat_target = create_material('defaultLitTransparency', base_color=[0.0,0.5,1.0,0.5], thickness=1.0, transmission=0.5, absorption_color=[0.5, 0.5, 0.5])
 	for index, m in enumerate(point_targets):
 		name = 'box' + str(index)
-		
-		vertices = m.vertices.copy()  # Make a copy of the vertices array
-		faces = m.faces.copy()        # Make a copy of the faces array
-		o3d_mesh = o3d.geometry.TriangleMesh()
-		o3d_mesh.vertices = o3d.utility.Vector3dVector(vertices)
-		o3d_mesh.triangles = o3d.utility.Vector3iVector(faces)
-			
-		geom = {'name': name, 'geometry': o3d_mesh, 'material': mat_tar}
+		o3d_mesh = convert_mesh(m)			
+		geom = {'name': name, 'geometry': o3d_mesh, 'material': mat_target}
 		geoms.append(geom)
-	'''
- 
+	
 	o3d.visualization.draw(geoms) # , window_name="LiDAR simulation")
-
 	# o3d.visualization.draw_geometries([o3d_mesh, intersection_points, ray_lines])
+
+	# save intersection
+	output_pcd = o3d.geometry.PointCloud()
+	for index, intersection_points in enumerate(intersection_points_set):
+		output_pcd += intersection_points
+	o3d.io.write_point_cloud(args.output, output_pcd)
 
 if __name__ == '__main__':
 	try:
